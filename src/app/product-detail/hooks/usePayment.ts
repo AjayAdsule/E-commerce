1bf899a94f9d7ec7822d438a22c7a54e4c36c13e
Ajay A Adsule <103304466+AjayAdsule@users.noreply.github.com>
2024-05-26 21:17:40 +0530

    make payment hook

--- src/app/product-detail/hooks/usePayment.ts
+++ src/app/product-detail/hooks/usePayment.ts
@@ -1,12 +1,10 @@
 import { loadStripe } from '@stripe/stripe-js';
+import { env } from 'process';
 import { clientApi } from '~/trpc/react';
 
 export default function usePayment() {
   const { mutateAsync: checkout } = clientApi.buyer.checkout.useMutation();
-
-  const stripePromise = loadStripe(
-    'pk_test_51O9m6ySJDbl4dha4XVFIOcpCsf6ox3iJp5IswxuxnZbHLOM6huZOxhMn61GAGkFRrDvauMzXx5FXcLipuEUn0ReX00zlqKIE7K',
-  );
+  const stripePromise = loadStripe(env.STRIPE_LOAD_SECRET ?? '');
   const processToPay = async ({
     name,
     amount,
--- src/env.js
+++ src/env.js
@@ -31,6 +31,7 @@ export const env = createEnv({
     STRIPE_SECRET_KEY: z.string(),
     STRIPE_PUBLISHED_KET: z.string(),
     STRIPE_WEBHOOK_SECRET: z.string(),
+    STRIPE_LOAD_SECRET: z.string(),
   },
 
   /**
@@ -59,6 +60,7 @@ export const env = createEnv({
     STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
     STRIPE_PUBLISHED_KET: process.env.STRIPE_PUBLISHED_KET,
     STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
+    STRIPE_LOAD_SECRET: process.env.STRIPE_LOAD_SECRET,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
