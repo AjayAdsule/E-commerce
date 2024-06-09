eda2ea3bec981484dc251e03454a8c3507a35f6c
Ajay A Adsule <103304466+AjayAdsule@users.noreply.github.com>
2024-06-09 13:04:54 +0530

    add navigation for navbar cart icon

--- src/components/common/NavbarActions.tsx
+++ src/components/common/NavbarActions.tsx
@@ -4,14 +4,15 @@ import React from 'react';
 import { ThemeBtn } from './ThemeBtn';
 import { Search, ShoppingCart, UserRound } from 'lucide-react';
 import { clientApi } from '~/trpc/react';
+import { useRouter } from 'next/navigation';
 
 const NavbarActions = () => {
   const { data } = clientApi.buyer.getCart.useQuery();
-
+  const router = useRouter();
   return (
     <>
       <Search size={18} strokeWidth={1} className='text-secondary-foreground' />
-      <button className='relative'>
+      <button className='relative' onClick={() => router.push('/cart')}>
         <ShoppingCart size={18} strokeWidth={1} className='text-secondary-foreground' />
         <div className='absolute bottom-0 h-5 w-5 rounded-full bg-primary text-sm text-white'>
           {data?.length}
